d9aaa2b363fcef6c7a882feca87c8ad5518a5cf5
Sutanshu <sutanshuraj@yahoo.com>
2024-10-18 09:05:38 +0530

    Added Docker and requirements.txt File.

---
+++ app/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from app.model.model import predict_pipeline
+from app.model.model import __version__ as model_version
+
+app = FastAPI()
+
+class textInput(BaseModel):
+	text : str
+
+class predictOutput(BaseModel):
+	language : str
+
+
+@app.get('/')
+def home():
+	return {"Health Check" : "OK", "Version" : model_version}
+
+
+@app.post('/predict', response_model=predictOutput)
+def predict(payload : textInput):
+	language = predict_pipeline(payload.text)
+	return {"language" : language}
+
