b770c0489140ab2bb79d6677a3f8db51cb4b3b4f
Sutanshu <sutanshuraj@yahoo.com>
2024-10-19 22:35:08 +0530

    Added Redis Cache.

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
+import redis
 from fastapi import FastAPI
 from pydantic import BaseModel
 from app.model.model import predict_pipeline
 from app.model.model import __version__ as model_version
 
 app = FastAPI()
+r = redis.Redis(host="redis", port=6379)
 
 class textInput(BaseModel):
 	text : str
@@ -15,6 +17,12 @@ class predictOutput(BaseModel):
 @app.get('/')
 def home():
 	return {"Health Check" : "OK", "Version" : model_version}
+
+
+@app.get('/unique')
+def unique():
+	r.incr("Unique")
+	return {"Unique" : r.get("Unique")}
 
 
 @app.post('/predict', response_model=predictOutput)
