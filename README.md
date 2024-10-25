f03b876c41515c813b19a717732eab3cf189e95d
Sutanshu <sutanshuraj@yahoo.com>
2024-10-25 17:44:22 +0530

    Add Docker and Docker-Compose Files.

---
+++ README.md
@@ -0,0 +1,5 @@
+### Reference:
+
+1. [Deploy Flask with Gunicorn and Nginx.](https://bit.ly/4e0ejgY)
+2. [Create PyPI Package](https://bit.ly/48i1qxL)
+3. [Python Package Setup](https://bit.ly/3YrYWZc)
--- app/main.py
+++ app/main.py
@@ -1,14 +1,13 @@
-import redis
 from fastapi import FastAPI
 from pydantic import BaseModel
 from app.model.model import predict_pipeline
 from app.model.model import __version__ as model_version
 
 app = FastAPI()
-r = redis.Redis(host="redis", port=6379)
 
 class textInput(BaseModel):
 	text : str
+
 
 class predictOutput(BaseModel):
 	language : str
@@ -17,12 +16,6 @@ class predictOutput(BaseModel):
 @app.get('/')
 def home():
 	return {"Health Check" : "OK", "Version" : model_version}
-
-
-@app.get('/unique')
-def unique():
-	r.incr("Unique")
-	return {"Unique" : r.get("Unique")}
 
 
 @app.post('/predict', response_model=predictOutput)
